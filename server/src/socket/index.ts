e7de1410d41e8eee27a8de1248c2b6a99981219b
Gel Balano <gel@mashupgarage.com>
2026-03-19 06:13:35 +0800

    feat(02-02): register gameHandler alongside roomHandler
    - Imported gameHandler in socket/index.ts
    - Registered gameHandler(io, socket) after roomHandler in connection handler

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,11 +1,13 @@
 import { Server } from 'socket.io';
 import { roomHandler } from './handlers/roomHandler';
+import { gameHandler } from './handlers/gameHandler';
 
 export function setupSocket(io: Server) {
   io.on('connection', (socket) => {
     console.log(`Player connected: ${socket.id}`);
 
     roomHandler(io, socket);
+    gameHandler(io, socket);
 
     socket.on('disconnect', () => {
       console.log(`Player disconnected: ${socket.id}`);
